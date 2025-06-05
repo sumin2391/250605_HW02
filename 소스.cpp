7e2b43ab62d8dca42c67ae28203a2f34c2fed91c
sumin2391 <baesumin0211@gmail.com>
2025-06-05 16:06:36 +0900

    메인 cout 수정

--- 소스.cpp
+++ 소스.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 using namespace std;
 
+class Animal
+{
+public:
+	char move();
+	void makeSound();
+};
+class Monster
+{
+public:
+	char move();
+	void sound();
+	void attack();
+};
+
+class Dog : public Animal {};
+class Cat : public Animal {};
+class Cow : public Animal {};
+
+
 int main() {
 	int Ro = 0;
 
 	for (int i = 0; i < 10; i++)
 	{
 		Ro += i;
-		std::cout << "hello world \n\n";
+		std::cout << "죄송합니다.. 아직 못했어요..모르겠어요.. \n\n";
 	}
 	return 0;
 }
