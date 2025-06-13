f0eef01299e8919282c28d7081870fcda76240e9
sumin2391 <baesumin0211@gmail.com>
2025-06-13 15:25:38 +0900

    copy & paste

--- 소스.cpp
+++ 소스.cpp
@@ -1,32 +1,76 @@
 #include <iostream>
-using namespace std;
 
-class Animal
-{
+class Animal {
 public:
-	char move();
-	void makeSound();
-};
-class Monster
-{
-public:
-	char move();
-	void sound();
-	void attack();
+    Animal() {
+        std::cout << "  [Animal 생성자 호출]" << std::endl;
+    }
+    virtual ~Animal() {
+        std::cout << "  [Animal 소멸자 호출]" << std::endl;
+    }
+    virtual void makeSound() const = 0;
 };
 
-class Dog : public Animal {};
-class Cat : public Animal {};
-class Cow : public Animal {};
+class Dog : public Animal {
+public:
+    Dog() {
+        std::cout << "    ㄴ Dog 생성자 호출" << std::endl;
+    }
+    ~Dog() {
+        std::cout << "    ㄴ Dog 소멸자 호출" << std::endl;
+    }
+    void makeSound() const override {
+        std::cout << "멍멍! (Woof!)" << std::endl;
+    }
+};
 
+class Cat : public Animal {
+public:
+    Cat() {
+        std::cout << "    ㄴ Cat 생성자 호출" << std::endl;
+    }
+    ~Cat() {
+        std::cout << "    ㄴ Cat 소멸자 호출" << std::endl;
+    }
+    void makeSound() const override {
+        std::cout << "야옹~ (Meow~)" << std::endl;
+    }
+};
+
+class Cow : public Animal {
+public:
+    Cow() {
+        std::cout << "    ㄴ Cow 생성자 호출" << std::endl;
+    }
+    ~Cow() {
+        std::cout << "    ㄴ Cow 소멸자 호출" << std::endl;
+    }
+    void makeSound() const override {
+        std::cout << "음메~ (Moo~)" << std::endl;
+    }
+};
 
 int main() {
-	int Ro = 0;
+    const int NUM_ANIMALS = 3;
+    Animal* animals[NUM_ANIMALS];
 
-	for (int i = 0; i < 10; i++)
-	{
-		Ro += i;
-		std::cout << "죄송합니다.. 아직 못했어요..모르겠어요.. \n\n";
-	}
-	return 0;
+    std::cout << "--- 동물 객체 동적 생성 시작 ---" << std::endl;
+    animals[0] = new Dog();
+    animals[1] = new Cat();
+    animals[2] = new Cow();
+    std::cout << "--- 동물 객체 동적 생성 완료 ---\n" << std::endl;
+
+    std::cout << "--- 모든 동물이 소리를 냅니다 ---" << std::endl;
+    for (int i = 0; i < NUM_ANIMALS; ++i) {
+        animals[i]->makeSound();
+    }
+    std::cout << "--------------------------------\n" << std::endl;
+
+    std::cout << "--- 동물 객체 메모리 해제 시작 ---" << std::endl;
+    for (int i = 0; i < NUM_ANIMALS; ++i) {
+        delete animals[i];
+    }
+    std::cout << "--- 동물 객체 메모리 해제 완료 ---" << std::endl;
+
+    return 0;
 }
